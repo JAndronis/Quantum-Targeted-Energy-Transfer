d65309b384abc3f6f7cdd80691ab1bf53ae6bdf1
JAndronis <iasona98@gmail.com>
2021-12-24 20:38:13 +0200

    added analytical code to main python file

--- PINN.py
+++ PINN.py
@@ -1,16 +1,20 @@
 #%%
+from re import T
 import sys
 assert sys.version_info >= (3, 5)
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import itertools
+import multiprocessing as mp
+import pandas as pd
+from functools import partial
 
 import tensorflow as tf
 assert tf.__version__ >= "2.0"
 from tensorflow import keras
 from tensorflow.keras.layers import Dense
-
 
 # Where to save the figures
 PROJECT_ROOT_DIR = "."
@@ -23,6 +27,45 @@ def save_fig(fig_id, tight_layout=True, fig_extension="jpg", resolution=300):
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def write_data(data, destination, name_of_file):
+    df = pd.DataFrame(data = data)
+    _destination = os.path.join(destination, name_of_file)
+    np.savetxt(_destination, df)
+
+def read_2D_data(name_of_file):
+    X,Y = [],[]
+    for line in open(name_of_file, 'r'):
+        lines = [i for i in line.split()]
+        X.append(float(lines[0]))
+        Y.append(float(lines[1]))
+    return X,Y
+
+def read_1D_data(name_of_file):
+    data = []
+    for line in open(name_of_file, 'r'):
+        lines = [i for i in line.split()]
+        data.append(float(lines[0]))
+    return data
+
+def construct_Hamiltonians(chiA,chiD):
+  H,H_uncoupling = np.zeros((N + 1, N + 1), dtype=float),np.zeros((N + 1, N + 1), dtype=float)
+  
+  # Step 1a) Complete problem
+  # i bosons at the donor
+  for i in range(N + 1):
+    for j in range(N + 1):
+      # First term from interaction
+      if i == j - 1: H[i][j] = -coupling_lambda * np.sqrt((i + 1) * (N - i))
+      # Second term from interaction
+      if i == j + 1:H[i][j] = -coupling_lambda * np.sqrt(i * (N - i + 1))
+        # Term coming from the two independent Hamiltonians
+      if i == j: H[i][j] = omegaD * i + 0.5 * chiD * i ** 2 + omegaA * (N - i) + 0.5 * chiA * (N - i) ** 2
+
+  # Step 1b) Uncoupling problem
+  np.fill_diagonal(H_uncoupling,np.diagonal(H))
+
+  return H,H_uncoupling
 
 #%%
 class Opt_PertTheory(tf.keras.Model):
@@ -191,4 +234,139 @@ class RL_Test(tf.keras.Model):
     figure, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(12,12))
     ax.plot_surface(XA, XD, Z, cmap='rainbow')
     plt.show()
+# %%
+def bosons_at_donor_analytical(max_t,eigvecs,eigvals,initial_state):
+  coeff_c = np.zeros(N+1,dtype=float)
+  for i in range(N+1): 
+    coeff_c[i] = np.vdot(eigvecs[:,i], initial_state)
+
+  j_vectors = np.identity(N+1)
+
+  coeff_b = np.zeros(eigvecs.shape)
+  for j in range(N+1):
+    for i in range(N+1):
+      coeff_b[j,i] = np.vdot(j_vectors[:, j], eigvecs[:, i])
+
+  avg_N = np.zeros(max_t+1,dtype=float)
+  t = 0
+
+  while True and t <= max_t:
+    sum_m = 0
+    for m in range(N+1):
+      sum_i = 0
+      for i in range(N+1):
+        sum_i += coeff_c[i]*coeff_b[m,i]*np.exp(-1j*eigvals[i]*t)
+
+      sum_k = 0
+      for k in range(N+1):
+        sum_k += coeff_c[k].conj()*coeff_b[m,k].conj()*np.exp(1j*eigvals[k]*t)*sum_i
+    
+      sum_m += sum_k*m
+    #print("\rt={}".format(t), end = "")
+    avg_N[t] = sum_m
+    t += 1
+
+  return avg_N
+
+def mp_bosons_at_donor_analytical(max_t,eigvecs,eigvals,initial_state):
+  coeff_c = np.zeros(N+1,dtype=float)
+  for i in range(N+1): 
+    coeff_c[i] = np.vdot(eigvecs[:,i], initial_state)
+
+  j_vectors = np.identity(N+1)
+
+  coeff_b = np.zeros(eigvecs.shape)
+  for j in range(N+1):
+    for i in range(N+1):
+      coeff_b[j,i] = np.vdot(j_vectors[:, j], eigvecs[:, i])
+
+  avg_N = np.zeros(max_t+1,dtype=float)
+  exp_time = range(0, max_t+1)
+
+  # for t in exp_time:
+  #   with mp.Pool(4) as pool:
+      
+  
+  # print(zip(eigvals, time, coeff_c, coeff_b))
+
+  for t in exp_time:
+    sum_m = _mp_avg_N_calc_m(eigvals, t, coeff_c, coeff_b, N)
+    #print("\rt={}".format(t), end = "")
+    avg_N[t] = sum_m
+
+  return avg_N
+
+def _mp_avg_N_calc_m(eigvals, t, coeff_c, coeff_b, N):
+  sum_j = 0
+  for j in range(N+1):
+    sum_i = sum(coeff_c[i]*coeff_b[j,i]*np.exp(-1j*eigvals[i]*t) for i in range(N+1))
+    sum_k = sum(coeff_c[k].conj()*coeff_b[j,k].conj()*np.exp(1j*eigvals[k]*t)*sum_i for k in range(N+1))
+    sum_j += sum_k*j
+  return sum_j
+
+
+if __name__ == "__main__":
+  N = 12
+  omegaA,omegaD = 3,-3
+  chiA,chiD = -0.5,0.5
+  coupling_lambda = 0.001
+
+  cwd = os.getcwd()
+  new_data = f"{cwd}/new_data"
+  try:
+    os.mkdir(new_data)
+  except OSError as error:
+    print(error)
+    while True:
+        query = input("Directory exists, replace it? [y/n]")
+        fl_1 = query[0].lower() 
+        if query == '' or not fl_1 in ['y','n']: 
+            print('Please answer with yes or no')
+        else: break
+    if fl_1 == 'n': sys.exit(0)
+
+  dir_name = f"coupling-{coupling_lambda}"
+  data_dest = os.path.join(new_data, dir_name)
+  print("Writing values in:", data_dest)
+  try:
+    os.mkdir(data_dest)
+  except OSError as error:
+    print(error)
+    while True:
+        query = input("Directory exists, replace it? [y/n]")
+        fl_2 = query[0].lower() 
+        if query == '' or not fl_2 in ['y','n']: 
+            print('Please answer with yes or no')
+        else: break
+    if fl_2 == 'n': sys.exit(0)
+
+  def mp_execute(chiA,chiD, data_dir):
+    # ----------- Step 1: Construct the Hamiltonian matrices of the problem -----------
+    H, H_uncoupling  = construct_Hamiltonians(chiA,chiD)
+
+    # ----------- Step 2: Solve the eigenvalue problem -----------
+    eigenvalues, eigenvectors = np.linalg.eigh(H)
+    #Diagonal matrix. Easy to compute eigenvalues
+    # eigenvalues_uncoupling, eigenvectors_uncoupling = linalg.eigh(H_uncoupling)
+
+    #----------- Step 3: Initial state ----------- 
+    initial_state = np.zeros(N+1,dtype=float)
+    for n in range(N+1): initial_state[n] = np.exp(-(N-n)**2)
+    initial_state = initial_state / np.linalg.norm(initial_state)
+    t_max = 2000
+    t_span = np.linspace(0,t_max,t_max+1)
+    avg_ND_analytical = mp_bosons_at_donor_analytical(max_t=t_max,eigvecs=eigenvectors,eigvals=eigenvalues,initial_state=initial_state)
+    # print(f'\nExecution time ={round(time.time() - start_time,4)} seconds')
+    title_file = f'ND_analytical-λ={coupling_lambda}-χA={chiA}-χD={chiD}.txt'
+    write_data(avg_ND_analytical, destination=data_dir, name_of_file=title_file)
+
+  t1 = time.time()
+  mp_execute(chiA, chiD, data_dest)
+  t2 = time.time()
+  dt = t2-t1
+  print(f"Code took:{dt}secs to run")
+
+  df = pd.read_csv(os.path.join(data_dest, os.listdir(data_dest)[0]))
+  df.plot()
+  save_fig("average_number_of_bosons")
 # %%
