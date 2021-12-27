5e549f2e4d6e43f4d47fd56f10596df1559d624c
Jason Andronis <iasona98@gmail.com>
2021-12-27 12:04:32 +0200

    added new file

---
+++ src/tet/write_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import os
+
+def write_data(data, destination, name_of_file):
+    df = pd.DataFrame(data = data)
+    _destination = os.path.join(destination, name_of_file)
+    np.savetxt(_destination, df)
