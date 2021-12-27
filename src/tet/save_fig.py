00016a8fd81d9eccc6ea3ba9073eb62400aebd6b
Jason Andronis <iasona98@gmail.com>
2021-12-27 14:14:51 +0200

    renamed and adjusted some values

--- src/tet/save_fig.py
+++ src/tet/save_fig.py
@@ -6,7 +6,7 @@ PROJECT_ROOT_DIR = "."
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images")
 os.makedirs(IMAGES_PATH, exist_ok=True)
 
-def save_fig(fig_id, tight_layout=True, fig_extension="jpg", resolution=300):
+def saveFig(fig_id, tight_layout=True, fig_extension="jpg", resolution=300):
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
     print("Saving figure", fig_id)
     if tight_layout:
--- src/tet/write_data.py
+++ src/tet/write_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-def write_data(data, destination, name_of_file):
+def writeData(data, destination, name_of_file):
     df = pd.DataFrame(data = data)
     _destination = os.path.join(destination, name_of_file)
     np.savetxt(_destination, df)
