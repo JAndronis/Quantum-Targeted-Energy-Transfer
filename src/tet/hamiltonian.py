6e28872dd5a041d6192c84c96a9de250b4c2d7b9
Jason Andronis <iasona98@gmail.com>
2021-12-27 14:09:27 +0200

    added uncoupled hamiltonian

--- src/tet/hamiltonian.py
+++ src/tet/hamiltonian.py
@@ -1,6 +1,7 @@
 import numpy as np
+import sys
 
-def construct_Hamiltonians(chiA, chiD, coupling_lambda, omegaA, omegaD, max_N):
+def createHamiltonian(chiA, chiD, coupling_lambda, omegaA, omegaD, max_N):
     '''
     Creates the hamiltonian of the dimer system
     INPUTS:
@@ -16,14 +17,28 @@ def construct_Hamiltonians(chiA, chiD, coupling_lambda, omegaA, omegaD, max_N):
     
     H = np.zeros((max_N + 1, max_N + 1), dtype=float)
 
-    # i bosons at the donor
-    for i in range(max_N + 1):
-        for j in range(max_N + 1):
-            # First term from interaction
-            if i == j - 1: H[i][j] = -coupling_lambda * np.sqrt((i + 1) * (max_N - i))
-            # Second term from interaction
-            if i == j + 1: H[i][j] = -coupling_lambda * np.sqrt(i * (max_N - i + 1))
-            # Term coming from the two independent Hamiltonians
-            if i == j: H[i][j] = omegaD * i + 0.5 * chiD * i ** 2 + omegaA * (max_N - i) + 0.5 * chiA * (max_N - i) ** 2
+    while True:
+        query = input('Is the system coupled? [y/n] ')
+        fl_1 = query[0].lower() 
+        if query == '' or not fl_1 in ['y','n']: 
+            print('Please answer with yes or no')
+        else: break
+
+    if fl_1 == 'y': 
+        # i bosons at the donor
+        for i in range(max_N + 1):
+            for j in range(max_N + 1):
+                # First term from interaction
+                if i == j - 1: H[i][j] = -coupling_lambda * np.sqrt((i + 1) * (max_N - i))
+                # Second term from interaction
+                if i == j + 1: H[i][j] = -coupling_lambda * np.sqrt(i * (max_N - i + 1))
+                # Term coming from the two independent Hamiltonians
+                if i == j: H[i][j] = omegaD * i + 0.5 * chiD * i ** 2 + omegaA * (max_N - i) + 0.5 * chiA * (max_N - i) ** 2
+
+    if fl_1 == 'n':
+        for i in range(max_N + 1):
+            for j in range(max_N + 1):
+                # Term coming from the two independent Hamiltonians
+                if i == j: H[i][j] = omegaD * i + 0.5 * chiD * i ** 2 + omegaA * (max_N - i) + 0.5 * chiA * (max_N - i) ** 2
 
     return H
