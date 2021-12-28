507e4276eb26388e211a7248ad169c945476009c
JAndronis <iasona98@gmail.com>
2021-12-28 21:07:06 +0200

    included adjustable path in function

--- src/tet/saveFig.py
+++ src/tet/saveFig.py
@@ -1,12 +1,11 @@
 import os
 import matplotlib.pyplot as plt
 
-# Where to save the figures
-PROJECT_ROOT_DIR = "."
-IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images")
-os.makedirs(IMAGES_PATH, exist_ok=True)
-
-def saveFig(fig_id, tight_layout=True, fig_extension="jpg", resolution=300):
+def saveFig(fig_id, destination, tight_layout=True, fig_extension="jpg", resolution=300):
+    # Where to save the figures
+    PROJECT_ROOT_DIR = destination
+    IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images")
+    os.makedirs(IMAGES_PATH, exist_ok=True)
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
     print("Saving figure", fig_id)
     if tight_layout:
