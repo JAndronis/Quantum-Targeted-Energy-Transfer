8dc2782b43316356d333d8335133aaedac9d0d7e
JAndronis <iasona98@gmail.com>
2021-12-29 01:10:38 +0200

    made a new helper func

--- src/tet/data_process.py
+++ src/tet/data_process.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import shutil
+import sys
 
 def writeData(data, destination, name_of_file):
     df = pd.DataFrame(data = data)
@@ -30,3 +31,16 @@ def compress(zip_files, destination):
     if zip_files:
         shutil.make_archive(base_name=f"{destination}-zipped", format='zip')
         shutil.rmtree(path=destination)
+
+def createDir(destination):
+    try:
+        os.mkdir(destination)
+    except OSError as error:
+        print(error)
+        while True:
+            query = input("Directory exists, replace it? [y/n] ")
+            fl_1 = query[0].lower() 
+            if query == '' or not fl_1 in ['y','n']: 
+                print('Please answer with yes or no')
+            else: break
+        if fl_1 == 'n': sys.exit(0)
