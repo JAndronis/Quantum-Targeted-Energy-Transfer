3715007f364c3a7f24a142058a374c6f5154729a
JAndronis <iasona98@gmail.com>
2021-12-30 01:46:09 +0200

    new ver and forgot to include a return

--- src/tet/execute.py
+++ src/tet/execute.py
@@ -30,6 +30,8 @@ def execute(chiA, chiD, coupling_lambda, omegaA, omegaD, max_N, max_t, data_dir)
                                                 eigvecs=eigenvectors,
                                                 eigvals=eigenvalues,
                                                 initial_state=initial_state).computeAverageComb()
+        
+        return avg_ND_analytical
 
     else:
         problemHamiltonian = Hamiltonian.Hamiltonian(chiA, chiD, coupling_lambda, omegaA, omegaD, max_N).createHamiltonian()
@@ -42,3 +44,4 @@ def execute(chiA, chiD, coupling_lambda, omegaA, omegaD, max_N, max_t, data_dir)
                                                 initial_state=initial_state).computeAverage()                                              
         title_file = f'ND_analytical-λ={coupling_lambda}-t_max={max_t}-χA={chiA}-χD={chiD}.txt'
         data_process.writeData(data=avg_ND_analytical, destination=data_dir, name_of_file=title_file)
+        return avg_ND_analytical
--- tests/package_test.py
+++ tests/package_test.py
@@ -1,3 +1,4 @@
+#%%
 import shutil
 import sys
 assert sys.version_info >= (3,6)
@@ -16,10 +17,10 @@ if __name__ == "__main__":
     max_N = 12
     omegaA, omegaD = 3, -3
     # chiA, chiD = -0.5, 0.5
-    coupling_lambda = 0.1
+    coupling_lambda = 1
     t_max = 2000
-    xA = np.linspace(-5, 5, 10)
-    xD = np.linspace(-5, 5, 10)
+    xA = np.linspace(-5, 5, 4)
+    xD = np.linspace(-5, 5, 4)
 
     cwd = os.getcwd()
     data = f"{cwd}/data"
@@ -40,32 +41,34 @@ if __name__ == "__main__":
     zip_files = False
     count_it = 0
 
+    b = []
     t1 = time.time()
     for chiA in xA:
         for chiD in xD:
             count_it +=1
             print("\rCombination {} out of {}: (chiA,chiD) = ({},{})".format(count_it,len(xA)*len(xD),round(chiA,4),round(chiD,4)), end = " ")
-            execute.execute(chiA=chiA, 
-                            chiD=chiD, 
-                            coupling_lambda=coupling_lambda, 
-                            omegaA=omegaA, 
-                            omegaD=omegaD, 
-                            max_N=max_N, 
-                            max_t=t_max, 
-                            data_dir=data_dest)
+            b.append(execute.execute(chiA=chiA,
+                                     chiD=chiD, 
+                                     coupling_lambda=coupling_lambda, 
+                                     omegaA=omegaA, 
+                                     omegaD=omegaD, 
+                                     max_N=max_N, 
+                                     max_t=t_max, 
+                                     data_dir=data_dest))
     t2 = time.time()
     dt = t2-t1
     print(f"Code took: {dt:.3f}secs to run")
+    b = np.array(b)
 
     t1 = time.time()
-    execute.execute(chiA=xA, 
-                    chiD=xD, 
-                    coupling_lambda=coupling_lambda, 
-                    omegaA=omegaA, 
-                    omegaD=omegaD, 
-                    max_N=max_N, 
-                    max_t=t_max, 
-                    data_dir=data_dest)
+    a = execute.execute(chiA=xA, 
+                        chiD=xD, 
+                        coupling_lambda=coupling_lambda, 
+                        omegaA=omegaA, 
+                        omegaD=omegaD, 
+                        max_N=max_N, 
+                        max_t=t_max, 
+                        data_dir=data_dest)
     t2 = time.time()
     dt = t2-t1
     print(f"Code took: {dt:.3f}secs to run")
@@ -73,20 +76,21 @@ if __name__ == "__main__":
     data_process.compress(zip_files=zip_files, destination=data_dest)
     if zip_files: sys.exit(0)
 
-    data_analytical = []
-    mimimums_ND = np.zeros(shape=(len(xA),len(xD)) )
-    plt.figure(figsize=(8,8))
-    for i in range(len(xA)):
-        for j in range(len(xD)):
-            title_analytical = f'ND_analytical-λ={coupling_lambda}-t_max={t_max}-χA={xA[i]}-χD={xD[j]}.txt'
-            data_analytical_case = data_process.read_1D_data(destination=data_dest, name_of_file=title_analytical)
-            mimimums_ND[i][j] = min(data_analytical_case)
-            data_analytical.append([ data_analytical_case,xA[i],xD[j]])
+    # data_analytical = []
+    # mimimums_ND = np.zeros(shape=(len(xA),len(xD)) )
+    # plt.figure(figsize=(8,8))
+    # for i in range(len(xA)):
+    #     for j in range(len(xD)):
+    #         title_analytical = f'ND_analytical-λ={coupling_lambda}-t_max={t_max}-χA={xA[i]}-χD={xD[j]}.txt'
+    #         data_analytical_case = data_process.read_1D_data(destination=data_dest, name_of_file=title_analytical)
+    #         mimimums_ND[i][j] = min(data_analytical_case)
+    #         data_analytical.append([ data_analytical_case,xA[i],xD[j]])
 
-    plt.imshow(mimimums_ND,cmap = 'gnuplot',extent=[min(xD),max(xD),max(xA),min(xA)])
-    plt.xlabel('xD')
-    plt.ylabel('xA')
-    plt.colorbar()
-    plt.title(f'tmax = {t_max}, points χA, χD = {len(xA), len(xD)}, λ={coupling_lambda}, ωA={omegaA}, ωD={omegaD}')
-    title_heatmap = f'heatmap_tmax{t_max}_pointsxA:{len(xA)}_pointsxD{len(xD)}_λ={coupling_lambda}.pdf'
-    saveFig.saveFig(title_heatmap, t_dir_path)
+    # plt.imshow(mimimums_ND,cmap = 'gnuplot',extent=[min(xD),max(xD),max(xA),min(xA)])
+    # plt.xlabel('xD')
+    # plt.ylabel('xA')
+    # plt.colorbar()
+    # plt.title(f'tmax = {t_max}, points χA, χD = {len(xA), len(xD)}, λ={coupling_lambda}, ωA={omegaA}, ωD={omegaD}')
+    # title_heatmap = f'heatmap_tmax{t_max}_pointsxA:{len(xA)}_pointsxD{len(xD)}_λ={coupling_lambda}.pdf'
+    # saveFig.saveFig(title_heatmap, t_dir_path)
+# %%
