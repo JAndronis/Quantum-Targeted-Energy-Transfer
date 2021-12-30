b17200185f4c46f68df3f651e48f306723cd1354
JAndronis <iasona98@gmail.com>
2021-12-30 02:59:22 +0200

    various changes

--- tests/package_test.py
+++ tests/package_test.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     # chiA, chiD = -0.5, 0.5
     coupling_lambda = 1
     t_max = 2000
-    xA = np.linspace(-5, 5, 4)
-    xD = np.linspace(-5, 5, 4)
+    xA = np.linspace(-5, 5, 100)
+    xD = np.linspace(-5, 5, 100)
 
     cwd = os.getcwd()
     data = f"{cwd}/data"
@@ -41,56 +41,54 @@ if __name__ == "__main__":
     zip_files = False
     count_it = 0
 
-    b = []
     t1 = time.time()
     for chiA in xA:
         for chiD in xD:
             count_it +=1
             print("\rCombination {} out of {}: (chiA,chiD) = ({},{})".format(count_it,len(xA)*len(xD),round(chiA,4),round(chiD,4)), end = " ")
-            b.append(execute.execute(chiA=chiA,
-                                     chiD=chiD, 
-                                     coupling_lambda=coupling_lambda, 
-                                     omegaA=omegaA, 
-                                     omegaD=omegaD, 
-                                     max_N=max_N, 
-                                     max_t=t_max, 
-                                     data_dir=data_dest))
+            execute.execute(chiA=chiA,
+                            chiD=chiD, 
+                            coupling_lambda=coupling_lambda, 
+                            omegaA=omegaA, 
+                            omegaD=omegaD, 
+                            max_N=max_N, 
+                            max_t=t_max, 
+                            data_dir=data_dest)
     t2 = time.time()
     dt = t2-t1
     print(f"Code took: {dt:.3f}secs to run")
-    b = np.array(b)
 
-    t1 = time.time()
-    a = execute.execute(chiA=xA, 
-                        chiD=xD, 
-                        coupling_lambda=coupling_lambda, 
-                        omegaA=omegaA, 
-                        omegaD=omegaD, 
-                        max_N=max_N, 
-                        max_t=t_max, 
-                        data_dir=data_dest)
-    t2 = time.time()
-    dt = t2-t1
-    print(f"Code took: {dt:.3f}secs to run")
+    # t1 = time.time()
+    # execute.execute(chiA=xA, 
+    #                 chiD=xD, 
+    #                 coupling_lambda=coupling_lambda, 
+    #                 omegaA=omegaA, 
+    #                 omegaD=omegaD, 
+    #                 max_N=max_N, 
+    #                 max_t=t_max, 
+    #                 data_dir=data_dest)
+    # t2 = time.time()
+    # dt = t2-t1
+    # print(f"Code took: {dt:.3f}secs to run")
 
     data_process.compress(zip_files=zip_files, destination=data_dest)
     if zip_files: sys.exit(0)
 
-    # data_analytical = []
-    # mimimums_ND = np.zeros(shape=(len(xA),len(xD)) )
-    # plt.figure(figsize=(8,8))
-    # for i in range(len(xA)):
-    #     for j in range(len(xD)):
-    #         title_analytical = f'ND_analytical-λ={coupling_lambda}-t_max={t_max}-χA={xA[i]}-χD={xD[j]}.txt'
-    #         data_analytical_case = data_process.read_1D_data(destination=data_dest, name_of_file=title_analytical)
-    #         mimimums_ND[i][j] = min(data_analytical_case)
-    #         data_analytical.append([ data_analytical_case,xA[i],xD[j]])
+    data_analytical = []
+    mimimums_ND = np.zeros(shape=(len(xA),len(xD)) )
+    plt.figure(figsize=(8,8))
+    for i in range(len(xA)):
+        for j in range(len(xD)):
+            title_analytical = f'ND_analytical-λ={coupling_lambda}-t_max={t_max}-χA={xA[i]}-χD={xD[j]}.txt'
+            data_analytical_case = data_process.read_1D_data(destination=data_dest, name_of_file=title_analytical)
+            mimimums_ND[i][j] = min(data_analytical_case)
+            data_analytical.append([ data_analytical_case,xA[i],xD[j]])
 
-    # plt.imshow(mimimums_ND,cmap = 'gnuplot',extent=[min(xD),max(xD),max(xA),min(xA)])
-    # plt.xlabel('xD')
-    # plt.ylabel('xA')
-    # plt.colorbar()
-    # plt.title(f'tmax = {t_max}, points χA, χD = {len(xA), len(xD)}, λ={coupling_lambda}, ωA={omegaA}, ωD={omegaD}')
-    # title_heatmap = f'heatmap_tmax{t_max}_pointsxA:{len(xA)}_pointsxD{len(xD)}_λ={coupling_lambda}.pdf'
-    # saveFig.saveFig(title_heatmap, t_dir_path)
+    plt.imshow(mimimums_ND,cmap = 'gnuplot',extent=[min(xD),max(xD),max(xA),min(xA)])
+    plt.xlabel('xD')
+    plt.ylabel('xA')
+    plt.colorbar()
+    plt.title(f'tmax = {t_max}, points χA, χD = {len(xA), len(xD)}, λ={coupling_lambda}, ωA={omegaA}, ωD={omegaD}')
+    title_heatmap = f'heatmap_tmax{t_max}_pointsxA:{len(xA)}_pointsxD{len(xD)}_λ={coupling_lambda}.pdf'
+    saveFig.saveFig(title_heatmap, t_dir_path)
 # %%
