931b52001a817d6fff49153c911f21794235ef95
JAndronis <iasona98@gmail.com>
2022-01-03 14:19:57 +0200

    test for import

--- src/tet/__init__.py
+++ src/tet/__init__.py
@@ -3,3 +3,7 @@ from . import data_process
 from . import execute
 from . import Hamiltonian
 from . import saveFig
+
+from execute import execute
+from Hamiltonian import Hamiltonian
+from AvgBosons import AvgBosons
