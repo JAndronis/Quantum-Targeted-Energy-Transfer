61dde26246c6c9fddc997bcdb552521227454b9c
JAndronis <iasona98@gmail.com>
2022-01-04 00:43:04 +0200

    moved stuff to main and added return query

--- tests/rl-test.py
+++ tests/rl-test.py
@@ -11,31 +11,36 @@ class RLModel(tf.keras.Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-if __name__=="__main__":
-    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+def main():
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.1f}".format(x)})
+    
     max_N = 12
     omegaA, omegaD = 3, -3
-    # chiA, chiD = -0.5, 0.5
-    coupling_lambda = 1
+    chiA, chiD = -0.5, 0.5
+    coupling_lambda = 0.001
     t_max = 2000
-    xA = np.linspace(-5, 5, 10)
-    xD = np.linspace(-5, 5, 10)
 
-    cwd = os.getcwd()
-    data = f"{cwd}/data"
+    xA = np.linspace(-6, 6, 50)
+    xD = np.linspace(-6, 6, 50)
 
-    coupling_dir = f"coupling-{coupling_lambda}"
-    coupling_dir_path = os.path.join(data, coupling_dir)
+    return_query = True
 
-    t_dir = f"tmax-{t_max}"
-    t_dir_path = os.path.join(coupling_dir_path, t_dir)
+    if not return_query:
+        cwd = os.getcwd()
+        data = f"{cwd}/data"
 
-    data_dest = os.path.join(t_dir_path, "avg_N")
+        coupling_dir = f"coupling-{coupling_lambda}"
+        coupling_dir_path = os.path.join(data, coupling_dir)
 
-    tet.data_process.createDir(data)
-    tet.data_process.createDir(coupling_dir_path)
-    tet.data_process.createDir(t_dir_path)
-    tet.data_process.createDir(data_dest)
+        t_dir = f"tmax-{t_max}"
+        t_dir_path = os.path.join(coupling_dir_path, t_dir)
+
+        data_dest = os.path.join(t_dir_path, "avg_N")
+
+        tet.data_process.createDir(data)
+        tet.data_process.createDir(coupling_dir_path)
+        tet.data_process.createDir(t_dir_path)
+        tet.data_process.createDir(data_dest)
 
     test_data = tet.Execute(chiA=xA, 
                             chiD=xD, 
@@ -45,28 +50,33 @@ if __name__=="__main__":
                             max_N=max_N, 
                             max_t=t_max, 
                             data_dir=data_dest,
-                            return_data=True).executeGrid()
-                            
-    df = pd.DataFrame(test_data)
-    XA, XD = np.meshgrid(xA, xD)
+                            return_data=return_query).executeGrid()
 
-    counter = 0
-    test_z = np.zeros(len(xA)*len(xD))
-    for i in enumerate(test_data): 
-        test_z[i[0]] = min(i[1])
-        counter += 1
-    test_z = test_z.reshape(len(xA), len(xD))
+    if return_query:                        
+        df = pd.DataFrame(test_data)
+        XA, XD = np.meshgrid(xA, xD)
 
-    figure, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(12,12))
-    plot = ax.plot_surface(XA, XD, test_z, cmap='gnuplot')
-    ax.set_xlabel('xD')
-    ax.set_ylabel('xA')
-    plt.show()
+        counter = 0
+        test_z = np.zeros(len(xA)*len(xD))
+        for i in enumerate(test_data): 
+            test_z[i[0]] = min(i[1])
+            counter += 1
+        test_z = test_z.reshape(len(xA), len(xD))
 
-    plt.contourf(test_z,cmap = 'gnuplot',extent=[min(xD),max(xD),max(xA),min(xA)], levels=50)
-    plt.xlabel('xD')
-    plt.ylabel('xA')
-    plt.colorbar()
-    plt.show()
+        figure, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(12,12))
+        plot = ax.plot_surface(XA, XD, test_z, cmap='gnuplot')
+        ax.set_xlabel('xD')
+        ax.set_ylabel('xA')
+        plt.show()
+
+        plt.figure(figsize=(12,12))
+        plt.contourf(test_z,cmap = 'gnuplot',extent=[min(xD),max(xD),max(xA),min(xA)], levels=50)
+        plt.xlabel('xD')
+        plt.ylabel('xA')
+        plt.colorbar()
+        plt.show()
+
+if __name__=="__main__":
+    main()
 
 # %%
