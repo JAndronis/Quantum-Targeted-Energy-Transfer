f38c7918a419dcb9d5acd9f1e5bf58c4c677bef7
Jason Andronis <iasona98@gmail.com>
2022-01-10 20:04:26 +0200

    removed redundant pandas dependency

--- src/tet/data_process.py
+++ src/tet/data_process.py
@@ -1,13 +1,11 @@
-import pandas as pd
 import numpy as np
 import os
 import shutil
 import sys
 
 def writeData(data, destination, name_of_file):
-    df = pd.DataFrame(data = data)
     _destination = os.path.join(destination, name_of_file)
-    np.savetxt(_destination, df.values)
+    np.savetxt(_destination, data)
 
 def read_1D_data(destination, name_of_file):
     _destination = os.path.join(destination, name_of_file)
