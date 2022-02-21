37fada8ac44ae91beee0f695a9eb5af1f45d7c3e
Jason Andronis <iasona98@gmail.com>
2022-02-21 20:03:58 +0200

    parameter change

--- tests/avgN_test.py
+++ tests/avgN_test.py
@@ -8,9 +8,9 @@ def main():
     # np.set_printoptions(formatter={'float': lambda x: "{0:0.1f}".format(x)})
     
     max_N = 4
-    omegaA, omegaD = 3, -3
+    omegaA, omegaD = 2, 1
     chiA, chiD = -1.5, 1.5
-    coupling_lambda = 1
+    coupling_lambda = 0.1
     t_max = 25
 
     xA = np.linspace(-4, 4, 100)
@@ -34,8 +34,8 @@ def main():
     tet.data_process.createDir(t_dir_path)
     tet.data_process.createDir(data_dest)
 
-    test_data = tet.Execute(chiA=chiA, 
-                            chiD=chiD, 
+    test_data = tet.Execute(chiA=xA, 
+                            chiD=xD, 
                             coupling_lambda=coupling_lambda, 
                             omegaA=omegaA, 
                             omegaD=omegaD, 
@@ -46,38 +46,37 @@ def main():
 
     # test_data = np.log(test_data)
 
-    plt.plot(test_data)
-    plt.show()
+    # plt.plot(test_data)
+    # plt.show()
 
-    # if return_query and np.ndim(test_data)>1:
-    #     XA, XD = np.meshgrid(xA, xD)
+    if return_query and np.ndim(test_data)>1:
+        XA, XD = np.meshgrid(xA, xD)
 
-    #     counter = 0
-    #     test_z = np.zeros(len(xA)*len(xD))
-    #     for i in enumerate(test_data): 
-    #         test_z[i[0]] = min(i[1])
-    #         counter += 1
-    #     test_z = test_z.reshape(len(xA), len(xD))
+        counter = 0
+        test_z = np.zeros(len(xA)*len(xD))
+        for i in enumerate(test_data): 
+            test_z[i[0]] = min(i[1])
+            counter += 1
+        test_z = test_z.reshape(len(xA), len(xD))
 
-    #     write_min_N(xA=XA, xD=XD, min_n=test_z, destination=data_dest, name_of_file='min_n_combinations')
+        write_min_N(xA=XA, xD=XD, min_n=test_z, destination=data_dest, name_of_file='min_n_combinations')
+        titl = f'N={max_N}, tmax = {t_max}, # points (χA, χD) = {len(xA), len(xD)}, λ={coupling_lambda}, ωA={omegaA}, ωD={omegaD}'
 
-    #     titl = f'N={max_N}, tmax = {t_max}, # points (χA, χD) = {len(xA), len(xD)}, λ={coupling_lambda}, ωA={omegaA}, ωD={omegaD}'
-
-    #     figure, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(12,12))
-    #     plot = ax.plot_surface(XA, XD, test_z, cmap='rainbow')
-    #     ax.set_xlabel(r"$\chi_{D}$", fontsize=20)
-    #     ax.set_ylabel(r"$\chi_{A}$", fontsize=20)
-    #     figure.colorbar(plot)
-    #     ax.set_title(titl, fontsize=20)
-    #     tet.saveFig(titl+' - 3dplot', t_dir_path)
-
-    #     figure2, ax2 = plt.subplots(figsize=(12,12))
-    #     plot2 = ax2.contourf(test_z,cmap = 'rainbow',extent=[min(xD),max(xD),min(-xA),max(-xA)], levels=50)
-    #     ax2.set_xlabel(r"$\chi_{D}$", fontsize=20)
-    #     ax2.set_ylabel(r"$\chi_{A}$", fontsize=20)
-    #     figure2.colorbar(plot2)
-    #     ax2.set_title(titl, fontsize=20)
-    #     tet.saveFig(titl+' - contourplot', t_dir_path)
+        figure, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(12,12))
+        plot = ax.plot_surface(XA, XD, test_z, cmap='rainbow')
+        ax.set_xlabel(r"$\chi_{D}$", fontsize=20)
+        ax.set_ylabel(r"$\chi_{A}$", fontsize=20)
+        figure.colorbar(plot)
+        ax.set_title(titl, fontsize=20)
+        tet.saveFig(titl+' - 3dplot', t_dir_path)
+    
+        figure2, ax2 = plt.subplots(figsize=(12,12))
+        plot2 = ax2.contourf(test_z,cmap = 'rainbow',extent=[min(xD),max(xD),min(-xA),max(-xA)], levels=50)
+        ax2.set_xlabel(r"$\chi_{D}$", fontsize=20)
+        ax2.set_ylabel(r"$\chi_{A}$", fontsize=20)
+        figure2.colorbar(plot2)
+        ax2.set_title(titl, fontsize=20)
+        tet.saveFig(titl+' - contourplot', t_dir_path)
 
 if __name__=="__main__":
     main()
