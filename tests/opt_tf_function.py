f351ec881f5a3bc947daed109ff5992060ddbb89
Jason Andronis <iasona98@gmail.com>
2022-03-03 13:18:56 +0200

    enable gpu memory growth

--- tests/opt_tf_function.py
+++ tests/opt_tf_function.py
@@ -14,8 +14,18 @@ assert tf.__version__ >= "2.0"
 import keras.backend as K
 
 # enable memory growth
-# gpu = tf.config.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(device=gpu[0], enable=True)
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
 
 # constants
 DTYPE = tf.float32
