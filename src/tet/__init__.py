b7fff020914ec95290e41541c860863233881149
Jason Andronis <iasona98@gmail.com>
2022-03-14 11:54:26 +0200

    updated init for extra imports

--- src/tet/__init__.py
+++ src/tet/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ['Execute', 'saveFig', 'Hamiltonian', 'data_process']
+__all__ = ['Execute', 'saveFig', 'Hamiltonian', 'data_process', 'AvgBosons', 'data_process']
 
 # Deprecated, only here for backwards compatibility
 from tet.Execute import Execute
 from tet.saveFig import saveFig
 from tet.Hamiltonian import Hamiltonian
+from tet.AvgBosons import AvgBosons
+import tet.data_process
