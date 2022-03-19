1f01f575822cb5cb26a2b395e11b94efbd110162
Jason Andronis <iasona98@gmail.com>
2022-03-19 16:13:57 +0200

    included custom training loop

--- tests/main_optimizer.py
+++ tests/main_optimizer.py
@@ -1,6 +1,5 @@
 from tet.Solver import solver
 import tensorflow as tf
-import tet.constants as constants
 
 if __name__=="__main__":
     # enable memory growth
@@ -16,21 +15,42 @@ if __name__=="__main__":
             # Memory growth must be set before GPUs have been initialized
             print(e)
 
+    done = False
+    edge = 1
+    iteration = 0
+    grid = 2
     # Make an initial search of the parameter space
     min_a, min_d, loss = solver(a_lims=[-3, 3],
                                 d_lims=[-3, 3],
-                                grid_size=2, 
+                                grid_size=grid, 
                                 case=0, 
                                 iterations=500, 
                                 learning_rate=0.1, 
                                 create_plot=True)
-    
-    a_min, a_max = min_a-1, min_a+1
-    d_min, d_max = min_d-1, min_d+1
-    xmin, xmax, loss = solver(a_lims=[a_min, a_max], 
-                              d_lims=[d_min, d_max], 
-                              grid_size=4, 
-                              case=1, 
-                              iterations=1000,
-                              learning_rate=0.01, 
-                              create_plot=True)
+    iteration += 1
+    if loss<=0.1:
+        print('TET!')
+        print(min_a, min_d, loss)
+        done = True
+    else:
+        while not done:
+            if iteration>3:
+                done = True
+                break
+            edge /= iteration
+            if grid<6: grid += 2
+            else: continue
+            a_min, a_max = min_a-edge, min_a+edge
+            d_min, d_max = min_d-edge, min_d+edge
+            min_a, min_d, loss = solver(a_lims=[a_min, a_max], 
+                                        d_lims=[d_min, d_max], 
+                                        grid_size=grid, 
+                                        case=iteration, 
+                                        iterations=1000,
+                                        learning_rate=0.01, 
+                                        create_plot=True)
+            iteration += 1
+            if loss<=0.1:
+                print('TET!')
+                print(min_a, min_d, loss)
+                done = True
