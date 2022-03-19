b98bfdd6e6d7b22d647a50ac4ff25c61e163dc32
Jason Andronis <iasona98@gmail.com>
2022-03-19 15:20:58 +0200

    included dict variable to dump specific dict

--- src/tet/constants.py
+++ src/tet/constants.py
@@ -11,10 +11,10 @@ def setConstant(key, value):
 def getConstant(key):
     return constants[key]
 
-def dumpConstants(path=os.getcwd()):
+def dumpConstants(dict=constants, path=os.getcwd()):
     _path = os.path.join(path, 'constants.json')
     with open(_path, 'w') as c:
-        json.dump(constants, c, indent=1)
+        json.dump(dict, c, indent=1)
         
 def loadConstants(path='constants.json'):
     with open(path, 'r') as c:
