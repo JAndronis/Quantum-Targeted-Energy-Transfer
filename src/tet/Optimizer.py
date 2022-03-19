b15c5249690a53e006f8262ce3b9c75eb3b11923
Jason Andronis <iasona98@gmail.com>
2022-03-19 15:23:03 +0200

    included train function in optimizer class to solve bug of multiple optimizers instatiating at the same time leading to a crash

--- src/tet/Optimizer.py
+++ src/tet/Optimizer.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 assert sys.version_info >= (3,6)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -11,8 +12,8 @@ assert tf.__version__ >= "2.0"
 import keras.backend as K
 
 from tet.data_process import createDir, writeData, read_1D_data
-from train import train
 from tet.saveFig import saveFig
+from loss import Loss
 import constants
 
 DTYPE = tf.float32
@@ -46,7 +47,7 @@ class Optimizer:
         self.plot = Plot
         self.iter = iterations
         self.lr = lr
-        
+        self.opt = tf.keras.optimizers.Adam()
         
     def __call__(self):
         if self.DataExist and self.plot: self.PlotResults()
@@ -58,9 +59,112 @@ class Optimizer:
             createDir(destination=self.CombinationPath,replace=True)
             self._train()
             if self.plot: self.PlotResults()
+            
+    @tf.function
+    def compute_loss(self, const):
+        return Loss(const=const).loss(self.xA, self.xD)
+
+    def get_grads(self, const):
+        with tf.GradientTape() as t:
+                t.watch([self.xA, self.xD])
+                loss = self.compute_loss(const)
+        grads = t.gradient(loss, [self.xA, self.xD])
+        del t
+        return grads, loss
+
+    @tf.function
+    def apply_grads(self, const):
+        grads, loss = self.get_grads(const)
+        self.opt.apply_gradients(zip(grads, [self.xA, self.xD]))
+        return loss
+
+    def train(self, ChiAInitial, ChiDInitial, const, max_iter=200, lr=0.01):
+        # Reset Optimizer
+        K.clear_session()
+        for var in self.opt.variables():
+            var.assign(tf.zeros_like(var))
+        K.set_value(self.opt.learning_rate, lr)
+        
+        LAMBDA = const['coupling']
+        OMEGA_A = const['omegaA']
+        OMEGA_D = const['omegaD']
+        MAX_N = const['max_N']
+        MAX_T = const['max_t']
+        
+        mylosses = []
+        tol = 1e-8
+        self.xA = tf.Variable(initial_value=ChiAInitial, trainable=True, dtype=DTYPE)
+        self.xD = tf.Variable(initial_value=ChiDInitial, trainable=True, dtype=DTYPE)
+        xA_best = tf.Variable(initial_value=0, dtype=DTYPE, trainable=False)
+        xD_best = tf.Variable(initial_value=0, dtype=DTYPE, trainable=False)
+        mylosses.append(MAX_N)
+        best_loss = MAX_N
+        counter = 0
+        d_data = []
+        a_data = []
+        a_error_count = 0
+        d_error_count = 0
+
+        t0 = time.time()
+        for epoch in range(max_iter):
+            xA_init = self.xA.numpy()
+            xD_init = self.xD.numpy()
+            loss = self.apply_grads(const)
+            if epoch%100 ==0: print(f'Loss:{loss.numpy()}, xA:{self.xA.numpy()}, xD:{self.xD.numpy()}, epoch:{epoch}')
+            
+            if loss.numpy()<=0.5:
+                K.set_value(self.opt.learning_rate, 0.01)
+            
+            errorA = np.abs(self.xA.numpy() - xA_init)
+            errorD = np.abs(self.xD.numpy() - xD_init)
+
+            mylosses.append(loss.numpy())
+            if mylosses[epoch+1] < min(list(mylosses[:epoch+1])):
+                xA_best.assign(self.xA.numpy())
+                xD_best.assign(self.xD.numpy())
+                best_loss = mylosses[epoch+1]
+
+            counter += 1
+            if counter%10 == 0:
+                d_data.append(self.xD.numpy())
+                a_data.append(self.xA.numpy())
+
+            if np.abs(loss.numpy()) < 0.1:
+                break
+            
+            if errorA < tol:
+                a_error_count += 1
+                if a_error_count > 2:
+                    print('Stopped training because of xA_new-xA_old =', errorA)
+                    break
+
+            if errorD < tol:
+                d_error_count += 1
+                if d_error_count > 2:
+                    print('Stopped training because of xD_new-xD_old =', errorA)
+                    break
+            
+        t1 = time.time()
+        dt = t1-t0
+
+        print("\nApproximate value of chiA:", xA_best.numpy(), 
+            "\nApproximate value of chiD:", xD_best.numpy(),
+            "\nLoss - min #bosons on donor:", best_loss,
+            "\nOptimizer Iterations:", self.opt.iterations.numpy(), 
+            "\nTraining Time:", dt,
+            "\n"+40*"-",
+            "\nParameters:",
+            "\nOmega_A:", OMEGA_A,
+            "| Omega_D:", OMEGA_D,
+            "| N:", MAX_N,
+            "| Total timesteps:", MAX_T,
+            "| Coupling Lambda:",LAMBDA,
+            "\n"+40*"-")
+        
+        return mylosses, a_data, d_data, xA_best.numpy(), xD_best.numpy()
     
     def _train(self):
-        mylosses, a_data, d_data, xA_best, xD_best = train(self.ChiAInitial, self.ChiDInitial, \
+        mylosses, a_data, d_data, xA_best, xD_best = self.train(self.ChiAInitial, self.ChiDInitial, \
             const=self.const, max_iter=self.iter, lr=self.lr)
         writeData(data=mylosses[1:], destination=self.CombinationPath, name_of_file='losses.txt')
         writeData(data=a_data, destination=self.CombinationPath, name_of_file='xAtrajectory.txt')
@@ -112,6 +216,3 @@ class Optimizer:
         ax2.legend(prop={'size': 15})
         ax2.set_title(titl, fontsize=20)
         saveFig(fig_id="contour", destination=self.CombinationPath)
-        
-if __name__=="__main__":
-    Optimizer(ChiAInitial=-1, ChiDInitial=1, DataExist=False, Case=0)()
