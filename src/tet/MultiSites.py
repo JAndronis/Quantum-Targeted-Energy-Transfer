2dad8c07edf7735f156105c26b248f4a3d95a556
Georgios Arapantonis <you@example.com>
2022-04-02 19:22:23 +0300

    Multi Sites-approach
    Second approach-Corrections: We need to obtain only one Hamiltonian and not every sub-Hamiltonian

--- src/tet/MultiSites.py
+++ src/tet/MultiSites.py
@@ -1,9 +1,12 @@
+from itertools import combinations
 import constraint
 import numpy as np
 from math import factorial
-
+from scipy.linalg import block_diag
 from sympy import HadamardPower
 from Hamiltonian import Hamiltonian
+import cmath
+from matplotlib import pyplot as plt
 
 #Solve the problem to find all the combinations
 class DeriveCombinations():
@@ -31,116 +34,166 @@ class DeriveCombinations():
         return solutions
 
 
-class CreateSubHamiltonian:
+class CreateHamiltonian:
 
-    def __init__(self,Nbosons,coupling_lambda,Sites):
-        self.NBosons = Nbosons
+    def __init__(self,maxN,coupling_lambda,Sites):
+        self.maxN = maxN
         self.Sites = Sites
         self.coupling_lambda = coupling_lambda
 
-        self.Nstates = int( factorial(self.NBosons+self.Sites-1)/( factorial(self.NBosons)*factorial(self.Sites-1) ) )
-        self.CombinationsBosons = DeriveCombinations(N=self.NBosons,f=self.Sites).SetProblem()
-        #print(self.CombinationsBosons)
+        self.Nstates = int( factorial(self.maxN+self.Sites-1)/( factorial(self.maxN)*factorial(self.Sites-1) ) )
+        self.CombinationsBosons = DeriveCombinations(N=self.maxN,f=self.Sites).SetProblem()
         self.StatesDictionary = dict(zip(np.arange(self.Nstates,dtype= int),self.CombinationsBosons))
         
-        self.omegas = [3,2]
-        self.chis = [0.5,1]
+        self.omegas = [-3,3]
+        self.chis =[0.5,-0.5]
 
 
+    #Find the Hnm element of the Hamiltonian
     def ConstructElement(self,n,m):
-        #Find the Hnm element of the Hamiltonian
-
         #First Term. Contributions due to the kronecker(n,m) elements
         Term1 = 0
-
         if n==m:
             for k in range(self.Sites):
                 Term1 += self.omegas[k]*self.StatesDictionary[m]["x{}".format(k)] +\
-                0.5*self.chis[k]*self.StatesDictionary[m]["x{}".format(k)]**2
+                0.5*self.chis[k]*(self.StatesDictionary[m]["x{}".format(k)])**2
 
         #Second Term.Various contributions
         
         Term2a,Term2b = 0,0
         for k in range(self.Sites-1):
-            #print(k)
             #Find the number of bosons
             nk = self.StatesDictionary[m]["x{}".format(k)]
             nkplusone = self.StatesDictionary[m]["x{}".format(k+1)]
             
-            #Important check
-            if (nkplusone != self.NBosons) and (nk != 0): 
-                #Find the new state/vol1
-                m1tildastate = self.StatesDictionary[m].copy()
-                m1tildastate["x{}".format(k)] = nk-1
-                m1tildastate["x{}".format(k+1)] = nkplusone+1
+            #Term 2a/Important check
+            if (nkplusone != self.maxN) and (nk != 0): 
+                m1TildaState = self.StatesDictionary[m].copy()
+                m1TildaState["x{}".format(k)] = nk-1
+                m1TildaState["x{}".format(k+1)] = nkplusone+1
 
-                m1tilda = list(self.StatesDictionary.keys())[list(self.StatesDictionary.values()).index(m1tildastate)]
+                m1TildaIndex = list(self.StatesDictionary.keys())[list(self.StatesDictionary.values()).index(m1TildaState)]
             
-                if m1tilda == n: Term2a += -self.coupling_lambda*np.sqrt((nkplusone+1)*nk)
+                if m1TildaIndex == n: Term2a += -self.coupling_lambda*np.sqrt((nkplusone+1)*nk)
             
-            if (nkplusone != 0) and (nk != self.NBosons): 
+            #Term 2b/Important check
+            if (nkplusone != 0) and (nk != self.maxN): 
                 #Find the new state/vol2
-                m2tildastate = self.StatesDictionary[m].copy()
-                m2tildastate["x{}".format(k)] = nk+1
-                m2tildastate["x{}".format(k+1)] = nkplusone-1
+                m2TildaState = self.StatesDictionary[m].copy()
+                m2TildaState["x{}".format(k)] = nk+1
+                m2TildaState["x{}".format(k+1)] = nkplusone-1
 
-                m2tilda = list(self.StatesDictionary.keys())[list(self.StatesDictionary.values()).index(m2tildastate)]
+                m2TildaIndex = list(self.StatesDictionary.keys())[list(self.StatesDictionary.values()).index(m2TildaState)]
 
-                if m2tilda == n: Term2b += -self.coupling_lambda*np.sqrt(nkplusone*(nk+1))
-
+                if m2TildaIndex == n: Term2b += -self.coupling_lambda*np.sqrt(nkplusone*(nk+1))
             
-        Term2 = Term2a + Term2b
-            
-        return Term1 + Term2
+        return Term1 + Term2a + Term2b
 
 
     def Execute(self):
-        Hsub = np.zeros(shape=(self.Nstates,self.Nstates),dtype=float )
+        H = np.zeros(shape=(self.Nstates,self.Nstates),dtype=float )
         for n in range(self.Nstates):
             for m in range(self.Nstates):
-                Hsub[n][m] = self.ConstructElement(n=n,m=m)
-        #print(Hsub)
-        return Hsub
+                H[n][m] = self.ConstructElement(n=n,m=m)
+        return H,self.CombinationsBosons 
 
 
-class CreateTotalHamiltonian:
+class Loss:
 
-    def __init__(self,maxN,coupling_lambda,Sites):
+    def __init__(self,H,States,maxN):
+        self.H = H
+        self.States = States
         self.maxN = maxN
-        self.coupling_lambda = coupling_lambda
-        self.Sites = Sites
+        self.Sites = len(self.States[0])
+        
+        self.dim = len(self.H)
+        self.StatesDict = dict(zip(np.arange(self.dim,dtype= int),self.States))
+        self.Identity = np.identity(n=self.dim)  
+        self.ccoeffs,self.bcoeffs = np.zeros(self.dim),np.zeros(self.dim)
+        
+    #Derive the initial state
+    def DeriveInitialState(self):
+
+        #Assign initially all the bosons at the donor
+        self.InitialState = self.States[0]
+        self.InitialState = dict.fromkeys(self.InitialState, 0)
+        self.InitialState[f'x{self.Sites-1}'] = self.maxN
+
+        #Find the index
+        self.InitialStateIndex = list(self.StatesDict.keys())[list(self.StatesDict.values()).index(self.InitialState)]
+        #Extra re-assign(;)
+        self.InitialState = self.Identity[self.InitialStateIndex]
+        #self.InitialState[self.InitialStateIndex] = self.maxN
+        self.InitialState =self.InitialState/np.linalg.norm(self.InitialState)
+        
+
+    def SetCoeffs(self):
+        self.DeriveInitialState()
+        
+        self.eigvals, self.eigvecs = np.linalg.eigh(self.H)
+        #b coeffs
+        self.bcoeffs = self.eigvecs
+        #c coeffs
+        for i in range(self.dim):self.ccoeffs[i] = np.vdot(self.eigvecs[:,i],self.InitialState)
 
 
+    def _computeAverageCalculation(self, t):
+        sum_j = 0
+        for j in range(self.dim):
+            sum_i = sum(self.ccoeffs*self.bcoeffs[j,:]*np.exp(-1j*self.eigvals*t))
+            sum_k = sum(self.ccoeffs.conj()*self.bcoeffs[j,:].conj()*np.exp(1j*self.eigvals*t)*sum_i)
+            sum_j += sum_k*j
+        return sum_j.real
 
-    def Create(self):
-        Hdiagonals = []
-        Hdiagonals.append([[0]])
-        for N in range(1,self.maxN+1):
-            #print(N)
-            Hsub = CreateSubHamiltonian(Nbosons=N,
-                                    coupling_lambda=self.coupling_lambda,
-                                    Sites=self.Sites).Execute()
-            Hdiagonals.append(Hsub)
+    def Execute(self):
+        Data = []
+        self.SetCoeffs()
+        #print(self.bcoeffs)
+        for t in range(tmax+1):
+            x = self._computeAverageCalculation(t)
+            Data.append(x)
 
-        return Hdiagonals
+        return Data
 
 
-def TotalHamiltonian(*args):
-    return block_diag(args)
+      
+
+
+    
+        
+
+
 #----------------------------------------------------------------------------------
-from scipy.linalg import block_diag
-maxN=3
+#np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
+#H_final = block_diag(*Hdiagonals)
+#CombinationsBosonsList = [item for sublist in CombinationsBosonsList for item in sublist]
+#Parameters of the problem
+maxN=12
 f=2
-coupling_lambda = 0.1
+coupling_lambda = 0.001
+tmax = 30000
 
-Hdiagonals = CreateTotalHamiltonian(maxN=maxN,coupling_lambda=coupling_lambda,Sites=f).Create()
-np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
-H_final = block_diag(*Hdiagonals)
-#H_final = TotalHamiltonian(Hdiagonals)
+#Create the Hamiltonian of the problem
+H,States = CreateHamiltonian(maxN=maxN,coupling_lambda=coupling_lambda,Sites=f).Execute()
+eigenvalues, eigenvectors = np.linalg.eigh(H)
 
-problemHamiltonian = Hamiltonian(chiA=1, chiD=0.5, coupling_lambda=coupling_lambda, 
-                                omegaA=2, omegaD=3, max_N=maxN).createHamiltonian()
+problemHamiltonian = Hamiltonian(chiA=-0.5, chiD=0.5, coupling_lambda=coupling_lambda, 
+                                omegaA = 3, omegaD = -3,max_N=maxN).createHamiltonian()
+
 eigenvalues1, eigenvectors1 = np.linalg.eigh(problemHamiltonian)
 
+"""
+for i in range(len(eigenvalues1)):
+    print(eigenvalues[i],eigenvalues1[i])
 
-eigenvalues2, eigenvectors2 = np.linalg.eigh(H_final)
+for i in range(eigenvectors1.shape[0]):
+    for j in range(eigenvectors1.shape[0]):
+        print(eigenvectors1[i][j],eigenvectors[i][j])
+    print('--------------------------------')
+"""
+#Data = Loss(H=problemHamiltonian,TotalCombinationsList=CombinationsBosonsList,maxN=maxN).Execute()
+Data = Loss(H=H,States=States,maxN=maxN).Execute()
+plt.plot(np.arange(0,tmax+1),Data)
+plt.show()
+
+
