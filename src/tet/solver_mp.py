3a772c9662e96196092207722b34b0b6d39dd0d9
Georgios Arapantonis <you@example.com>
2022-04-14 16:35:40 +0300

    Changes discussed
    Small changes

--- src/tet/solver_mp.py
+++ src/tet/solver_mp.py
@@ -1,4 +1,4 @@
-from itertools import product
+from itertools import combinations, product
 import numpy as np
 import os
 import gc
@@ -34,7 +34,7 @@ def getCombinations(a_lims, d_lims, const, method='bins', grid=2):
         extenti = (a_lims[0]-0.1, a_lims[1]+0.1)
         extentj = (d_lims[0]-0.1, d_lims[1]+0.1)
         
-        # produce bin edges
+        # produce bin edges.Default returning: H,xedges,yedges.
         _, *edges = np.histogram2d(data[:,0], data[:,1], bins=grid, range=(extenti, extentj))
         
         # create an indexed list of possible choices for initial guess
@@ -44,7 +44,7 @@ def getCombinations(a_lims, d_lims, const, method='bins', grid=2):
         data_and_bins = list(zip(data, hitbins))
         it = range(1, grid+1)
         Combinations = []
-        
+        #? Didn't get that
         for bin in list(product(it,it)):
             test_item = []
             for item in data_and_bins:
@@ -58,14 +58,15 @@ def getCombinations(a_lims, d_lims, const, method='bins', grid=2):
         # make a grid of uniformly distributed initial parameter guesses
         xa = np.linspace(a_lims[0], a_lims[1], const['resolution'])
         xd = np.linspace(d_lims[0], d_lims[1], const['resolution'])
-        Combinations = []
-        for comb in product(xa, xd):
-            Combinations.append(comb)
+        Combinations = list(product(xa,xd))
 
     return Combinations
 
-def solver_mp(xa_lims, xd_lims, const, grid=2, lr=0.1,\
-     epochs_bins=1000, epochs_grid=200, target_site='x0', data_path=os.path.join(os.getcwd(),'data')):
+def solver_mp(xa_lims, xd_lims, const, 
+              grid=2, lr=0.1,
+              epochs_bins=1000, epochs_grid=200, 
+              target_site='x0', 
+              data_path=os.path.join(os.getcwd(),'data')):
     """
     Function that utilizes multiple workers on the cpu to optimize the non linearity parameters for TET.
     It uses the Optimizer class to write trajectory data to multiple files so it can be parsed later if needed.
@@ -86,19 +87,17 @@ def solver_mp(xa_lims, xd_lims, const, grid=2, lr=0.1,\
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
     # create data directory to save results
-    data_path1 = data_path
-    createDir(destination=data_path1, replace_query=True)
+    createDir(destination=data_path, replace_query=True)
 
     # initialize helper parameters
     a_lims = xa_lims   # limits of xA guesses
     d_lims = xd_lims   # limits of xD guesses
     grid = grid
+
     _edge = [5, 4, 3, 2, 1, 0.5, 0.1]   # list of integers to decrease bin size by
-    iteration = 0
-    counter = 0
+    iteration,counter = 0,0
     done = False
     bin_choice = False
-    grid_choice = False
     min_a, min_d, min_loss = 0, 0, const['max_N']   # initializing min_loss to the maximum number
                                                     # ensures that the initial combinations of initial
                                                     # guesses will be done with the bin method
@@ -107,12 +106,13 @@ def solver_mp(xa_lims, xd_lims, const, grid=2, lr=0.1,\
     while not done and iteration<=5:
 
         # create directory of current iteration
-        data_path2 = os.path.join(data_path1, f'iteration_{iteration}')
+        data_path2 = os.path.join(data_path, f'iteration_{iteration}')
         createDir(destination=data_path2, replace_query=True)
 
         # if min loss is not small enough do a general search with the bin method
+        #? Why do we need the grid condition?
         if grid<=6 and min_loss>=const['max_N']-1.5:
-            if bin_choice and min_loss<=const['max_N']:
+            if bin_choice:
                 # if this method has already been picked increase bin number
                 if not grid==6:
                     edge = _edge[iteration]
@@ -123,7 +123,6 @@ def solver_mp(xa_lims, xd_lims, const, grid=2, lr=0.1,\
                     d_lims = [d_min,d_max]
                 else:
                     grid = 2
-                    edge = 1
                     iteration = 0
                     a_lims = xa_lims
                     d_lims = xd_lims
@@ -144,6 +143,7 @@ def solver_mp(xa_lims, xd_lims, const, grid=2, lr=0.1,\
             bin_choice = False
             Combinations = getCombinations(a_lims, d_lims, method='grid', const=const)
             iter = epochs_grid
+            #? Why do we need this?
             grid_choice = True
             print(10*'-',f'Iteration: {iteration}, Method: Grid, Jobs: {len(Combinations)}, a_lim: {a_lims}, d_lim: {d_lims}', 10*'-')
 
@@ -204,14 +204,15 @@ if __name__=="__main__":
     # Set globals
     constants.setConstant('max_N', 2)
     constants.setConstant('max_t', 200)
-    constants.setConstant('omegaA', 3)
-    constants.setConstant('omegaD', -3)
-    constants.setConstant('omegaMid', 3)
+    constants.setConstant('omegaA', -3)
+    constants.setConstant('omegaD', 3)
+    constants.setConstant('omegaMid', -3)
     constants.setConstant('coupling', 1)
     constants.setConstant('xMid', 0)
     constants.setConstant('sites', 3)
     constants.setConstant('resolution', 5)
     CONST = constants.constants
+    #? Why do we need that?
     constants.dumpConstants()
 
     solver_mp(xa_lims=[-5,5], xd_lims=[-5,5], const=CONST, target_site='x2')
