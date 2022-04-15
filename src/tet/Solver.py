a96b9b93dcd29950b312e9626bee84f90f36b23e
Jason Andronis <iasona98@gmail.com>
2022-04-15 14:23:00 +0300

    corrected a bunch of bugs and adjusted for testing

--- src/tet/Solver.py
+++ src/tet/Solver.py
@@ -5,13 +5,11 @@ import os
 import multiprocessing as mp
 
 from tet import constants
-from tet.Optimizer import Optimizer, mp_opt
-from tet.data_process import read_1D_data
+from Optimizer import Optimizer
+from tet.data_process import createDir, read_1D_data
 from tet.saveFig import saveFig
 
-
-
-def solver(a_lims, d_lims, grid_size, case, iterations=500, learning_rate=0.01, create_plot=False, multi=False):
+def solver(a_lims, d_lims, iterations=500, learning_rate=0.01, create_plot=False):
     const = constants.loadConstants()
     done = False
     
@@ -20,21 +18,11 @@ def solver(a_lims, d_lims, grid_size, case, iterations=500, learning_rate=0.01, 
     xd = np.linspace(d_lims[0], d_lims[1], const['resolution'])
     Combinations = list(product(xa,xd))
     
-    # where to save the data
-    data_path = os.path.join(os.getcwd(), f'data_optimizer_avgn_{case}')
-    
-    # check if data exists
-    if os.path.isdir(data_path):
-        if not os.listdir(data_path):
-            data_exists = False
-        else:    
-            data_exists = True
-    else:
-        data_exists = False
-    
     # Init an array to save chiAs/chiDs and resulting losses
     all_losses = np.ones((len(Combinations), 3))*const['max_N']
-    
+    p = os.path.join(os.getcwd(), f'data')
+    createDir(destination=p, replace_query=True)
+
     if create_plot:
         # Load Background
         min_n_path = os.path.join(os.getcwd(), 'data/coupling-'+str(const['coupling'])+'/tmax-'+\
@@ -52,26 +40,36 @@ def solver(a_lims, d_lims, grid_size, case, iterations=500, learning_rate=0.01, 
     
     for i,(ChiAInitial, ChiDInitial) in enumerate(Combinations):
         if not done:
+            # where to save the data
+            data_path = os.path.join(p, f'data_{i}')
+            
+            opt = Optimizer(target_site='x2',
+                            DataExist=False,
+                            data_path=data_path,
+                            const=const,
+                            lr=learning_rate,
+                            iterations=iterations)
+
+            # # Read Data from the i-th combination
+            # CombinationPath = os.path.join(data_path,f'combination_{i}')
+
+            # check if data exists
+            if os.path.isdir(data_path):
+                if not os.listdir(data_path):
+                    data_exists = False
+                else:    
+                    data_exists = True
+            else:
+                data_exists = False
+
             if not data_exists:
                 print('-'*20+'Combination:{} out of {}, Initial (xA,xD):({:.3f},{:.3f})'.\
                     format(i, len(Combinations)-1, ChiAInitial, ChiDInitial) + '-'*20)
                 
-            opt = Optimizer(ChiAInitial=ChiAInitial,
-                            ChiDInitial=ChiDInitial,
-                            DataExist=data_exists, 
-                            data_path=data_path,
-                            Case=i,
-                            const=const,
-                            Plot=False,
-                            lr=learning_rate,
-                            iterations=iterations)
-            opt()
-            
-            # Read Data from the i-th combination
-            CombinationPath = os.path.join(data_path,f'combination_{i}')
+            opt(ChiAInitial, ChiDInitial)
             
             # Load Data
-            loss_data = read_1D_data(destination=CombinationPath, name_of_file='losses.txt')
+            loss_data = read_1D_data(destination=data_path, name_of_file='losses.txt')
             a_init = ChiAInitial
             d_init = ChiDInitial
             a = const['xA']
@@ -79,8 +77,8 @@ def solver(a_lims, d_lims, grid_size, case, iterations=500, learning_rate=0.01, 
             all_losses[i] = np.array([a, d, np.min(loss_data)])
         
         if create_plot:
-            a = read_1D_data(destination=CombinationPath, name_of_file='xAtrajectory.txt')
-            d = read_1D_data(destination=CombinationPath, name_of_file='xDtrajectory.txt')
+            a = read_1D_data(destination=data_path, name_of_file='xAtrajectory.txt')
+            d = read_1D_data(destination=data_path, name_of_file='xDtrajectory.txt')
             # Plot trajectory and initial guess data
             x = np.array(np.array(d))
             y = np.array(np.array(a))
@@ -106,3 +104,21 @@ def solver(a_lims, d_lims, grid_size, case, iterations=500, learning_rate=0.01, 
     min_a, min_d = all_losses[np.argmin(all_losses[:,2]), 0], all_losses[np.argmin(all_losses[:,2]), 1]
     min_loss = all_losses[np.argmin(all_losses[:,2]), 2]
     return min_a, min_d, min_loss
+
+if __name__=="__main__":
+
+    # Set globals
+    constants.setConstant('max_N', 3)
+    constants.setConstant('max_t', 200)
+    constants.setConstant('omegaA', -3)
+    constants.setConstant('omegaD', 3)
+    constants.setConstant('omegaMid', -3)
+    constants.setConstant('coupling', 1)
+    constants.setConstant('xMid', 0)
+    constants.setConstant('sites', 3)
+    constants.setConstant('resolution', 5)
+    CONST = constants.constants
+    constants.dumpConstants()
+
+    solver(a_lims=[-5,5], d_lims=[-5,5])
+    exit(0)
