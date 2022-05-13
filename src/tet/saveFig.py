c607b2d9fb710c54c08c2a5c05700e7458718c5f
JAndronis <iasona98@gmail.com>
2022-05-13 12:39:15 +0300

    added silent option

--- src/tet/saveFig.py
+++ src/tet/saveFig.py
@@ -1,7 +1,7 @@
 import os
 import matplotlib.pyplot as plt
 
-def saveFig(fig_id, destination, tight_layout=True, fig_extension="jpg", resolution=300):
+def saveFig(fig_id, destination, tight_layout=True, fig_extension="jpg", resolution=300, silent=False):
     """Function to save a figure in a specified directory.
 
     Args:
@@ -15,7 +15,8 @@ def saveFig(fig_id, destination, tight_layout=True, fig_extension="jpg", resolut
     IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images")
     os.makedirs(IMAGES_PATH, exist_ok=True)
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
-    print("Saving figure", fig_id)
+    if not silent:
+        print("Saving figure", fig_id)
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
