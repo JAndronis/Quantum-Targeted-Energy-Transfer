1c9ab01758220b61193e3190fac00ea03099430c
Georgios Arapantonis <96702482+geoarapa@users.noreply.github.com>
2022-05-14 17:43:36 +0300

    Update solver_mp.py
    Added documentation-comments.

--- src/tet/solver_mp.py
+++ src/tet/solver_mp.py
@@ -1,23 +1,24 @@
-from itertools import product
+
 import numpy as np
 import os
 import gc
 import sys
 import time
 import multiprocessing as mp
+import constants
 
-from sympy import solve_linear
-
+from itertools import combinations, product
 from Optimizer import mp_opt
 from data_process import createDir
-import constants
 from constants import solver_params,TensorflowParams
+
+
 #!Creates a list of initial guess pairs to be fed to an optimizer call
-def getCombinations(a_lims, d_lims, const, method='bins', grid=2):
+def getCombinations(TrainableVarsLimits, method='bins', grid=2):
     """
     Args:
-        * a_lims (list): List of 2 elements that contains the minimum and maximum xA's to use.
-        * d_lims (list): List of 2 elements that contains the minimum and maximum xD's to use.
+        * TrainableVarsLimits (Dictionary): The keys are the nonlinearity parameters of each site and the values 
+        * include a list with the limits of the said variable.
         * const (dict): Dictionary of problem parameters.
         * method (str, optional): Method to use for creating Combinations list. Defaults to 'bins'.
         grid (int, optional): Number of times to split the parameter space. Defaults to 2.
@@ -31,54 +32,60 @@ def getCombinations(a_lims, d_lims, const, method='bins', grid=2):
     if method not in method_list:
         raise ValueError('Provided method not in list of supported methods [\'grid\', \'bins\']')
 
+
     if method=='bins':
-        xa = np.linspace(a_lims[0], a_lims[1], 100)
-        xd = np.linspace(d_lims[0], d_lims[1], 100)
-        data = np.array(list(product(xa,xd)))
-        
-        # Extent of bins needs to be a bit smaller than parameter range
-        extenti = (a_lims[0]-0.1, a_lims[1]+0.1)
-        extentj = (d_lims[0]-0.1, d_lims[1]+0.1)
-        
-        # Produce bin edges.Default returning: H,xedges,yedges.
-        _, *edges = np.histogram2d(data[:,0], data[:,1], bins=grid, range=(extenti, extentj))
-        
-        # create an indexed list of possible choices for initial guess
-        hitx = np.digitize(data[:, 0], edges[0])
-        hity = np.digitize(data[:, 1], edges[1])
-        hitbins = list(zip(hitx, hity))
-        data_and_bins = list(zip(data, hitbins))
-        it = range(1, grid+1)
-        Combinations = []
-        for bin in list(product(it,it)):
-            test_item = []
-            for item in data_and_bins:
-                if item[1]==bin:
-                    test_item.append(item[0])
-            # choose initial conditions and append them to the combination list
-            choice = np.random.choice(list(range(len(test_item))))
-            Combinations.append(test_item[choice])
+        pass
+        """
+            xa = np.linspace(a_lims[0], a_lims[1], 100)
+            xd = np.linspace(d_lims[0], d_lims[1], 100)
+            data = np.array(list(product(xa,xd)))
+            
+            # Extent of bins needs to be a bit smaller than parameter range
+            extenti = (a_lims[0]-0.1, a_lims[1]+0.1)
+            extentj = (d_lims[0]-0.1, d_lims[1]+0.1)
+            
+            # Produce bin edges.Default returning: H,xedges,yedges.
+            _, *edges = np.histogram2d(data[:,0], data[:,1], bins=grid, range=(extenti, extentj))
+            
+            # create an indexed list of possible choices for initial guess
+            hitx = np.digitize(data[:, 0], edges[0])
+            hity = np.digitize(data[:, 1], edges[1])
+            hitbins = list(zip(hitx, hity))
+            data_and_bins = list(zip(data, hitbins))
+            it = range(1, grid+1)
+            Combinations = []
+            for bin in list(product(it,it)):
+                test_item = []
+                for item in data_and_bins:
+                    if item[1]==bin:
+                        test_item.append(item[0])
+                # choose initial conditions and append them to the combination list
+                choice = np.random.choice(list(range(len(test_item))))
+                Combinations.append(test_item[choice])
+            """
 
     elif method=='grid':
         # make a grid of uniformly distributed initial parameter guesses
-        xa = np.linspace(a_lims[0], a_lims[1], solver_params['Npoints'])
-        xd = np.linspace(d_lims[0], d_lims[1], solver_params['Npoints'])
-        Combinations = list(product(xa,xd))
+        TrainableSpans = [ np.linspace( TrainableVarsLimits[f'x{i}lims'][0], TrainableVarsLimits[f'x{i}lims'][1], solver_params['Npoints'])
+                            for i in TensorflowParams['train_sites'] ]
+
+        Combinations = list(product(*TrainableSpans))
 
     return Combinations
 
-def solver_mp(xa_lims, xd_lims, const, 
+def solver_mp(TrainableVarsLimits, const, 
               grid=2, lr=TensorflowParams['lr'],
               epochs_bins=solver_params['epochs_bins'], epochs_grid=solver_params['epochs_grid'], 
               target_site='x0', 
               data_path=os.path.join(os.getcwd(),'data')):
+
     """
     Function that utilizes multiple workers on the cpu to optimize the non linearity parameters for TET.
     It uses the Optimizer class to write trajectory data to multiple files so it can be parsed later if needed.
 
     Args:
-        * xa_lims (list): List of 2 elements. Contains the limits of the xA parameter space to search in.
-        * xd_lims (list): List of 2 elements. Contains the limits of the xD parameter space to search in.
+        * TrainableVarsLimits (Dictionary): The keys are the nonlinearity parameters of each site and the values 
+        * include a list with the limits of the said variable.
         * const (dict): Dictionary of system parameters that follows the convention used by the tet.constants() module.
         * grid (int, optional): Integer representing the number of times to split the parameter space. Defaults to 2.
         * lr (float, optional): Learning rate of the optimizer. Defaults to 0.1.
@@ -88,32 +95,39 @@ def solver_mp(xa_lims, xd_lims, const,
         * data_path (str, optional): Path to create the data directory. Defaults to cwd/data.
     """
 
-    # use cpu since we are doing parallelization on the cpu
+
+    #! Use cpu since we are doing parallelization on the cpu
     #os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-    # create data directory to save results
+    # Create data directory to save results
     createDir(destination=data_path, replace_query=True)
 
-    # initialize helper parameters
-    a_lims = xa_lims   # limits of xA guesses
-    d_lims = xd_lims   # limits of xD guesses
+    # Initialize helper parameters
+    lims = list(TrainableVarsLimits.values())
     grid = grid
 
-    _edge = [5, 4, 3, 2, 1, 0.5, 0.1]   # list of integers to decrease bin size by
-    iteration,counter = 0,0
+    _edge =  [0.1,0.5,1.5,2.5,3.0,3.5,4.0]  
+
+    # Control how many times loss is lower than the threshold having changed the limits
+    iteration = 0
+
+    # Count attempts based on the limits 
+    counter = 0
     done = False
     bin_choice = False
-    min_a, min_d, min_loss = 0, 0, const['max_N']   # initializing min_loss to the maximum number
+
+    #* An array to save the optimal parameters
+    OptimalVars, min_loss = np.zeros(len(TrainableVarsLimits)), const['max_N']   # initializing min_loss to the maximum number
                                                     # ensures that the initial combinations of initial
                                                     # guesses will be done with the bin method
 
     t0 = time.time()
-    while not done and iteration < 1:
+    while not done and iteration < 2:
 
-        # create directory of current iteration
+        # Create directory of current iteration
         data_path2 = os.path.join(data_path, f'iteration_{iteration}')
         createDir(destination=data_path2, replace_query=True)
-
+        """
         # if min loss is not small enough do a general search with the bin method
         # if grid<=6 and min_loss>=const['max_N']-1.5:
         #     if bin_choice:
@@ -145,56 +159,61 @@ def solver_mp(xa_lims, xd_lims, const,
             #     a_lims = [a_min,a_max]
             #     d_lims = [d_min,d_max]
         #bin_choice = False
-        Combinations = getCombinations(a_lims, d_lims, method='grid', const=const)
+        """
+        Combinations = getCombinations(TrainableVarsLimits, method='grid')
+    
         iter = epochs_grid
-        grid_choice = True
-        print(10*'-',f'Iteration: {iteration}, Method: Grid, Jobs: {len(Combinations)}, a_lim: {a_lims}, d_lim: {d_lims}', 10*'-')
+        #grid_choice = True
+        print(10*'-',f'Iteration: {iteration}, Method: Grid, Jobs: {len(Combinations)}, lims: {lims}', 10*'-')
 
         t2 = time.time()
-        # initialize processing pool
+        # Initialize processing pool
         pool = mp.Pool(max(mp.cpu_count()//2, 1))
 
-        # set input arg list for mp_opt() function
-        args = [(i, ChiAInitial, ChiDInitial, data_path2, const, target_site, lr, iter) for i, (ChiAInitial, ChiDInitial) in enumerate(Combinations)]
+        # Set input arg list for mp_opt() function
+        args = [(i, combination, data_path2, const, target_site, lr, iter) for i, (combination) in enumerate(Combinations)]
 
         try:
-            # run multiprocess map 
-            all_losses = pool.starmap_async(mp_opt, args).get(timeout=1000)
+            # Run multiprocess map 
+            _all_losses = pool.starmap_async(mp_opt, args).get()
 
-        except KeyboardInterrupt:
+        except KeyboardInterrupt():
             print('Keyboard Interrupt.')
-            pool.close()    # make sure to close pool so no more processes start
+            # Make sure to close pool so no more processes start
+            pool.close()    
             pool.join()
             gc.collect()
             sys.exit(1)
 
         finally:
-            pool.close()    # make sure to close pool so no more processes start
+            # Make sure to close pool so no more processes start
+            pool.close()    
             pool.join()
-            gc.collect()    # garbage collector
+            # Garbage collector
+            gc.collect()    
 
         t3 = time.time()
-        dt = t3-t2  # collect code run time for optimization
+        # Collect code run time for optimization
+        dt = t3-t2  
 
-        # gather results
-        all_losses = np.array(all_losses)
-        min_a, min_d = float(all_losses[np.argmin(all_losses[:,2]), 0]), float(all_losses[np.argmin(all_losses[:,2]), 1])
-        min_loss = float(all_losses[np.argmin(all_losses[:,2]), 2])
+        # Gather results
+        all_losses = np.array(_all_losses)
+        OptimalVars = [float(all_losses[np.argmin(all_losses[:,const['sites']]), i]) for i in range(const['sites']) ]
+        min_loss = float(all_losses[np.argmin(all_losses[:,const['sites']]), const['sites']])
         
-        # print results of run
-        print(f"Best parameters of tries: loss={min_loss}, xA={min_a}, xD={min_d}")
+        # Print results of run
+        print(f"Best parameters of tries: loss={min_loss}, OptimalVars = {OptimalVars}")
         print("Code run time: ", dt, " s")
 
         if min_loss<=const["max_N"]/2:
             counter += 1
             edge = _edge[iteration]
             #grid += 2
-            a_min, a_max = min_a-edge, min_a+edge
-            d_min, d_max = min_d-edge, min_d+edge
-            a_lims = [a_min,a_max]
-            d_lims = [d_min,d_max]
+    
+            lims = [ [ OptimalVars[i]-edge, OptimalVars[i]+edge ] for i in range(len(TrainableVarsLimits)) ]
+
         else:
-            const['Npoints'] += 1
+            solver_params['Npoints'] += 1
             lr += 0.1
 
         # advance iteration
@@ -202,38 +221,42 @@ def solver_mp(xa_lims, xd_lims, const,
 
         # if loss has not been reduced for more than 5 iterations stop
         if counter>=5 and min_loss>=CONST['max_N']/2:
-            print('Couldnt find TET')
+            print("Couldn't find TET")
             break
         
         # tet has been achieved no need to continue
         if float(min_loss)<=0.1:
             print('TET!')
-            print(f"xA: {min_a}, xD: {min_d}, loss: {min_loss}")
+
+    
+            print(f'OptimalParams:{OptimalVars}')
             done = True
             break
 
     t1 = time.time()
-    const['xA'] = str(min_a)
-    const['xD'] = str(min_d)
-    constants.dumpConstants(dict=const)
+
+    # Write best parameters to parameter json
+    const['chis'] = [f'{OptimalVars[i]}' for i in range(len(OptimalVars))]
+    constants.dumpConstants(dict=const, path=data_path)
+
     print('Total solver run time: ', t1-t0)
-    print(all_losses)
+
 
 if __name__=="__main__":
-
-    # # Set constants
-    # constants.setConstant('max_N', 3)
-    # constants.setConstant('max_t', 200)
-    # constants.setConstant('omegaA', -3)
-    # constants.setConstant('omegaD', 3)
-    # constants.setConstant('omegaMid', -3)
-    # constants.setConstant('coupling', 1)
-    # constants.setConstant('xMid', 0)
-    # constants.setConstant('sites', 3)
-    # constants.setConstant('Npoints', 6)
+    #! Import the constants of the problem
     CONST = constants.constants
-    constants.dumpConstants()
 
-    solver_mp(xa_lims=[-5,5], xd_lims=[-5,5], const=CONST, target_site=solver_params['target'])
+    #! Create a dictionary with the limits of each variable explored
+    keys = [ f'x{i}lims' for i in TensorflowParams['train_sites'] ] 
+    lims = [[-1.5,1.5]]*len(keys)
+    TrainableVarsLimits = dict(zip(keys,lims))
+
+    # Create data directory with the naming convention data_{unix time}
+    data_dir_name = f'data_{time.time_ns()}'
+    data = os.path.join(os.getcwd(), data_dir_name)
+
+    #! Call the solver function that uses multiprocessing(pointer _mp)
+    solver_mp(TrainableVarsLimits, const=CONST, target_site=solver_params['target'], data_path=data)
+
     exit(0)
 
