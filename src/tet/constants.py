d92d23b8fa49a135373ae41cc3b59aaa854c19e4
Georgios Arapantonis <96702482+geoarapa@users.noreply.github.com>
2022-10-20 17:27:46 -0400

    Update constants.py
    Add some comments, ignore

--- src/tet/constants.py
+++ src/tet/constants.py
@@ -5,21 +5,26 @@ from tensorflow import float32
 # -------------------------------------------------------------------#
 
 """
-Constants: A dictionary that defined the constant parameters of the problem
+system_constants: A dictionary that defines the constant parameters of the problem
+
 Documentation:
     * max_N: The number of bosons belonging to the system
+    * sites: The number of the system's oscillators. Usually denoted by f
     * max_t: Period of time to compute the loss function
     * omegas: An f-dimensional list with the frequency of the oscillators of the system
-    * chis: An f-dimensional with the non linearity parameters of the oscillators. Include the constant value of 
-    * the non trainable parameters and enter random values for the trainable ones.
-    * sites: The number of the oscillators of the system. Usually denoted by f.
+    * chis: An f-dimensional list with the nonlinearity parameters of the oscillators
+    * Include the constant value of the non trainable parameters and enter random values for the trainable ones
+    * coupling: The characteristic coupling parameter
+    * timesteps: Determines the steps in time for computing the loss function.
+    
 """
 system_constants = {'max_N': 3,
-             'max_t': 25, 
+             'sites': 3,
+             'max_t': 25,
+             'sites': 3, 
              'omegas': [-3,3,3],
              'chis': [1.5,0,-1.5],
              'coupling': 1,
-             'sites': 3,
              'timesteps': 30}
 
 # -------------------------------------------------------------------#
@@ -28,10 +33,11 @@ system_constants = {'max_N': 3,
 TensorflowParams: A dictionary that includes the parameters used for the commands of the Tensorflow library
 Documentation:
     * DTYPE: dtype of parameters
-    * lr: The default value of the learning rate of each optimizer.
+    * lr: The default value of the learning rate of each optimizer
     * iterations: The default number of maximum Iterations of each optimizer
-    * tol : The tolerance of each optimizer concerning the changes in the non-linearity parameters.
-    * train_sites: A list including the non-linearity parameters to be optimized. Begin counting from 0
+    * tol : The tolerance of each optimizer concerning the changes in the nonlinearity parameters
+    * train_sites: A list including the nonlinearity parameters to be optimized. Counting 
+    * ranges from 0 to f-1.
 """
 TensorflowParams = {'DTYPE': float32, 
                     'lr': 0.1, 
@@ -49,10 +55,11 @@ donor = 'x0'
 solver_params: A dictionary that defines a set of parameters used in the solver_mp.py file.
 Documentation:
     * methods: Must remain immutable. A list with the possible methods of setting the initial guesses of the optimizers.
-    * target: An integer ranging from 0 to f-1. It determines the site of which you desire to compute the loss function.
-    * Default value is f-1,meaning the acceptor. 
-    * Npoints: The num parameter in np.linspace. Given the limits of a trainable parameter, it determines in how many points
-    * will you split the interval
+    * target: Taking values of the format xk, where k is integer ranging from 0 to f-1. 
+    * It determines the site of which you desire to compute the loss function.Default value is x{f-1}, 
+    * referring to the acceptor. 
+    * Npoints: The num parameter in np.linspace. Given the limits of a trainable parameter, it determines the desnsity of 
+    * the 1D grid.
     * epochs_grid: The maximum iterations of each optimizer when using the grid method
     * epochs_bins: The maximum iterations of each optimizer when using the bins method
 
@@ -63,43 +70,56 @@ solver_params = {'methods': ['grid', 'bins'],
                 'epochs_grid':500,
                 'epochs_bins':1000}
 
-#! Create a dictionary with the limits of each variable explored
+
+
+# -------------------------------------------------------------------#
+"""
+Create a dictionary with the limits of each trainable nonlinearity parameter.
+"""
 keys = [f'x{i}lims' for i in TensorflowParams['train_sites']] 
 lims = [[-10,10]]*len(keys)
 TrainableVarsLimits = dict(zip(keys,lims))
 
-plotting_params = {'plotting_resolution': 100}
 
 # -------------------------------------------------------------------#
-
 """
-Helper functions
+Defining the resolution of the figures created
+"""
+plotting_params = {'plotting_resolution': 100}
+
+
+# -------------------------------------------------------------------#
+"""
+dumpConstants: Function that generates a json file from the dictionary provided.
 """
 
 def dumpConstants(dict=system_constants, path=os.getcwd(), name='constants'):
-    """Function that generates a json file from the dictionary provided.
-
-    Args:
-        dict (dict, optional): Dictionary to save. Defaults to system_constants.
-        path (str, optional): Path to save json file to. Defaults to os.getcwd().
-        name (str, optional): Name of file. Defaults to 'constants'.
+    """
+    Documentation:
+        * dict (dict, optional): Dictionary to save. Defaults to system_constants.
+        * path (str, optional): Path to save json file to. Defaults to os.getcwd().
+        * name (str, optional): Name of file. Defaults to 'constants'.
     """
     _path = os.path.join(path, name+'.json')
     with open(_path, 'w') as c:
         json.dump(dict, c, indent=1)
-        
-def loadConstants(path='system_constants.json'):
-    """Function that loads a json that was created with the dumpConstants function and writes the
-    contents to a dictionary. 
 
-    Args:
-        path (str, optional): Path of the file to load. Defaults to 'system_constants.json'.
+# -------------------------------------------------------------------#
+"""
+loadConstants: Function that loads a json that was created with the dumpConstants function and writes the
+contents to a dictionary.
+"""        
+def loadConstants(path='system_constants.json'):
+    """ 
+    Documentation:
+        * path (str, optional): Path of the file to load. Defaults to 'system_constants.json'.
 
     Returns:
-        dict: Dictionary containing the constants of the json file.
+        * dict: Dictionary containing the constants of the json file.
     """
     with open(path, 'r') as c:
         system_constants = json.load(c)
     return system_constants
 
 # -------------------------------------------------------------------#
+
