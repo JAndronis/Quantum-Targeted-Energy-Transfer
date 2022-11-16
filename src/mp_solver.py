c74e9561611bb447567bd19e6c442d2463cdcadc
JAndronis <iasona98@gmail.com>
2022-11-16 11:17:02 +0100

    added __name__=="__main__" clause

--- src/mp_solver.py
+++ src/mp_solver.py
@@ -1,42 +1,50 @@
-import tet.constants as constants
-import tet.saveFig as saveFig
-import numpy as np
-import os
-import shutil
-import sys
-import glob
-from os.path import exists
-import matplotlib.pyplot as plt
-from math import sqrt
-import warnings
 
-from tet.Optimizer import Optimizer
-import tet.constants
-from tet.solver_mp import solver_mp
+if __name__=="__main__":
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-const = {
-    "max_N": 3,
-    "max_t": 25,
-    "omegas": [
-        -3,
-        3
-    ],
-    "chis": [
-        -6.0,
-        6.0
-    ],
-    "coupling": 0.1,
-    "sites": 2,
-    "timesteps": 30,
-    "min_n": 1
-}
+    import tet.constants as constants
+    import tet.saveFig as saveFig
+    import numpy as np
+    import os
+    import shutil
+    import sys
+    import glob
+    from os.path import exists
+    import matplotlib.pyplot as plt
+    from math import sqrt
+    import warnings
 
-keys = [f'x{i}lims' for i in tet.constants.TensorflowParams['train_sites']] 
-lims = [[-10,10]]*len(keys)
-TrainableVarsLimits = dict(zip(keys,lims))
+    from tet.Optimizer import Optimizer
+    import tet.constants
+    from tet.solver_mp import solver_mp
 
-tet.constants.solver_params['Npoints'] = 20
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-opt = solver_mp(TrainableVarsLimits=TrainableVarsLimits, const=const, data_path='/home/ph4783/data/data', method='grid')
+    const = {
+        "max_N": 3,
+        "max_t": 25,
+        "omegas": [
+            -3,
+            3
+        ],
+        "chis": [
+            -6.0,
+            6.0
+        ],
+        "coupling": 0.1,
+        "sites": 2,
+        "timesteps": 30,
+        "min_n": 1
+    }
+
+    tet.constants.TensorflowParams['train_sites'] = [0, 1]
+    keys = [f'x{i}lims' for i in tet.constants.TensorflowParams['train_sites']] 
+    lims = [[-10,10]]*len(keys)
+    TrainableVarsLimits = dict(zip(keys,lims))
+
+    tet.constants.solver_params['Npoints'] = 2
+
+    opt = solver_mp(
+        TrainableVarsLimits=TrainableVarsLimits, const=const, 
+        data_path=os.path.join(os.getcwd(), 'data'), method='grid'
+    )
 
