f5936d51f931ffd08cc29d8d9cf4cfede41086a1
JAndronis <iasona98@gmail.com>
2023-02-15 13:31:23 +0100

    updated args, included option for multiprocessing

--- src/wrapper_solver.py
+++ src/wrapper_solver.py
@@ -5,6 +5,7 @@ import pathlib
 import os
 from datetime import datetime
 import argparse
+import multiprocessing as mp
 
 import tet.constants
 from tet.solver_mp import solver_mp
@@ -13,9 +14,9 @@ from tet.data_process import createDir
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="python3 array_solver.py")
-    parser.add_argument('-p', '--path', nargs='?', type=pathlib.Path, required=True)
-    # parser.add_argument('--id', type=int)
-    # parser.add_argument('--array-size', type=int)
+    parser.add_argument('-p', '--path', nargs='?', type=pathlib.Path, required=True, help='Path to create data dir.')
+    parser.add_argument('-c', '--constants', type=pathlib.Path, required=False, help='Path to constants json file.')
+    parser.add_argument('-n', '--ncpus', default=mp.cpu_count() // 2, type=int, required=False, help='Number of cpus to use in the process pool.')
 
     cmd_args = parser.parse_args()
     data_path = os.path.join(cmd_args.path, f'data_{datetime.now()}')
@@ -29,30 +30,25 @@ if __name__ == "__main__":
     except OSError:
         os.makedirs(data_path)
 
-    const = {
-        "max_N": 1,
-        "max_t": 25,
-        "omegas": [
-            -3,
-            3
-        ],
-        "chis": [
-            0,
-            0
-        ],
-        "coupling": 0.1,
-        "sites": 2,
-        "timesteps": 30,
-        "min_n": 1
-    }
+    if cmd_args.constants != None:
+        if not os.path.exists(cmd_args.constants):
+            raise OSError('Provided constants path does not exist.')
+        else:
+            const = tet.constants.loadConstants(cmd_args.constants)
+    else:
+        print('\n*No json file was provided, using default constants.*\n')
+        const = tet.constants.system_constants
 
+    # Set sites to optimize
     tet.constants.TensorflowParams['train_sites'] = [0, 1]
     keys = [f'x{k}lims' for k in tet.constants.TensorflowParams['train_sites']]
     lims = [[randint(-10, 0), randint(0, 10)], [randint(-10, 0), randint(0, 10)]]
     trainable_vars_lims = dict(zip(keys, lims))
 
-    solver_mp(
+    result = solver_mp(
         const=const, trainable_vars_limits=trainable_vars_lims, lr=0.1, beta_1=0.4, amsgrad=True,
-        data_path=data_path, method='bins', return_values=True, write_data=True
+        data_path=data_path, method='bins', write_data=True, cpu_count=cmd_args.ncpus
     )
+
+    print(result)
     
